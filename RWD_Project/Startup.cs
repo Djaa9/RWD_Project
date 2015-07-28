baef82aa74e6a9a8d607d50be72ad4d966e48f37
Johnny Poulsen <jp@thepage.dk>
2015-07-28 17:43:55 +0200

    Added fresh new ASP.NET Web Application using MVC and Web API

---
+++ RWD_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RWD_Project.Startup))]
+namespace RWD_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
